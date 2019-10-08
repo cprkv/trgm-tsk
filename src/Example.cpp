7c6ae7b06b8cd2540d44107fe1c413acb7754fa1
Vadim Chuprakov <mnz-rrh@ya.ru>
2019-10-08 05:20:16 +0500

    added implimentation of operator{=, +}, garbage constructor

--- src/Example.cpp
+++ src/Example.cpp
@@ -4,10 +4,18 @@
 int main( int argc, char** argv )
 {
 	using namespace trgm;
+
 	SimpleString s{ "abc" };
 	SimpleString s1{ std::move( s ) };
 	const SimpleString s3 = s + s1;
 	const SimpleString s4 = s3;
+	const SimpleString s5 = std::move( s1 );
 	std::cout << s4.CStr() << '\n';
+
+	const auto a1 = SimpleString{ "qwe" };
+	const auto a2 = SimpleString{ "rtytbd" };
+	const auto a3 = a1 + a2;
+	std::cout << a1.CStr() << '+' << a2.CStr() << '=' << a3.CStr() << '\n';
+
 	return 0;
 }
--- src/SimpleString.hpp
+++ src/SimpleString.hpp
@@ -12,16 +12,20 @@ namespace trgm
 	{
 	public:
 							SimpleString();
+								// initializes string of prefLength, containing garbage
+							SimpleString( size_t prefLength );
 		explicit			SimpleString( const char* );
 							SimpleString( const SimpleString& );
 							SimpleString( SimpleString&& ) noexcept;
 							~SimpleString();
 
-		size_t				Length() const	{ return length; }	// count of bytes, except \0
-		const char*			CStr() const	{ return chars; }	// c-style string
+		size_t				Length() const			{ return length; }	// count of bytes, except \0
+		const char*			CStr() const			{ return chars; }	// c-style string
 
 		SimpleString&		operator=( const SimpleString& );
 		SimpleString&		operator=( SimpleString&& ) noexcept;
+		char&				operator[]( size_t i ) 			{ return chars[i]; }
+		const char&			operator[]( size_t i ) const 	{ return chars[i]; }
 
 	private:
 		char*				chars;
