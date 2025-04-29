82b7dc4bee061fc1ea35a2106cd24b51662b41ff
cprkv <mnz-rrh@ya.ru>
2025-04-29 17:05:52 +0500

    "simplified" version

--- string/String.cpp
+++ string/String.cpp
@@ -1,10 +1,74 @@
 #include "String.hpp"
-#include "StringAlgo.hpp"
+#include <utility>	 // std::swap
 #include <algorithm> // std::min, std::max
 #include <cstddef>	 // size_t
 #include <cctype>	 // std::toupper
 
 using namespace trgm;
+
+// Task requires to implement it because <cstring> header is forbidden.
+static void MemoryCopy( char* dst, const char* src, size_t size )
+{
+	for( ; size--; *( dst++ ) = *( src++ ) );
+}
+
+// Task requires to implement it because <cstring> header is forbidden.
+static void MemorySet( char* dst, size_t size, char value )
+{
+	for( ; size--; *( dst++ ) = value );
+}
+
+// Task requires to implement it because <cstring> header is forbidden.
+static size_t CStrLength( const char* str )
+{
+	size_t length = 0;
+
+	if( str )
+		for( ; *str; length++, str++ );
+
+	return length;
+}
+
+static String Concatenate( const char* a, size_t aLength, const char* b, size_t bLength )
+{
+	String result( aLength + bLength, '\0' );
+	MemoryCopy( result.Data(), a, aLength );
+	MemoryCopy( result.Data() + aLength, b, bLength );
+	return result;
+}
+
+String::~String()
+{
+	free( m_buffer );
+}
+
+String::String( const String& o )
+{
+	Fill( o.Data(), o.Length() );
+}
+
+String& String::operator=( const String& o )
+{
+	if( &o != this )
+		Fill( o.Data(), o.Length() );
+	return *this;
+}
+
+String::String( String&& o )
+{
+	std::swap( m_buffer, o.m_buffer );
+	std::swap( m_length, o.m_length );
+}
+
+String& String::operator=( String&& o )
+{
+	if( &o != this )
+	{
+		std::swap( m_buffer, o.m_buffer );
+		std::swap( m_length, o.m_length );
+	}
+	return *this;
+}
 
 String::String( const char* cstr )
 {
@@ -13,10 +77,15 @@ String::String( const char* cstr )
 
 String& String::operator=( const char* o )
 {
-	size_t new_length = CStrLength( o );
-	Fill( o, new_length );
-	m_length = new_length;
+	Fill( o, CStrLength( o ) );
 	return *this;
+}
+
+String::String( size_t length, char value )
+{
+	m_buffer = static_cast<char*>( malloc( length ) );
+	m_length = length;
+	MemorySet( m_buffer, m_length, value );
 }
 
 String& String::operator+=( char c )
@@ -39,59 +108,46 @@ String& String::operator+=( const char* o )
 
 void String::Fill( const char* src, size_t srcSize, size_t position )
 {
-	const auto requiredSize = position + srcSize;
-	m_buffer.EnsureSize( requiredSize );
-	MemoryCopy( m_buffer.Data() + position, src, srcSize );
-	m_length = std::max( m_length, requiredSize );
+	size_t requiredSize = position + srcSize;
+
+	if( m_length == 0 && requiredSize > 0 )
+	{
+		m_buffer = static_cast<char*>( malloc( requiredSize ) );
+	}
+	else if( requiredSize > m_length )
+	{
+		m_buffer = static_cast<char*>( realloc( m_buffer, requiredSize ) );
+	}
+
+	MemoryCopy( m_buffer + position, src, srcSize );
+	m_length = requiredSize;
 }
 
 std::istream& trgm::operator>>( std::istream& s, String& str )
 {
-	if( !std::istream::sentry{ s, true } )
-		return s;
+	char buffer[1024]{ 0 };
 
-	auto state	 = static_cast<std::ios_base::iostate>( std::istream::goodbit );
-	bool changed = false;
+	if( s.getline( buffer, sizeof( buffer ) ) )
+		str = String( buffer );
+	else
+		str = String();
 
-	try // Some strange technique of using input streams (msvcrt).
-	{
-		for( auto c = s.rdbuf()->sgetc();; c = s.rdbuf()->snextc() )
-		{
-			auto character = std::istream::traits_type::to_char_type( c );
-
-			if( c == std::istream::traits_type::eof() )
-			{
-				state |= std::istream::eofbit;
-				break;
-			}
-			else if( std::istream::traits_type::eq( character, '\r' ) )
-			{
-				changed = true;
-				s.rdbuf()->sbumpc(); // skip, we dont need it
-			}
-			else if( std::istream::traits_type::eq( character, '\n' ) )
-			{
-				changed = true;
-				s.rdbuf()->sbumpc(); // skip, we dont need it
-				break;
-			}
-			else
-			{
-				str += character;
-			}
-		}
-	}
-	catch( ... )
-	{
-		s.setstate( std::istream::badbit );
-		throw;
-	}
-
-	if( !changed )
-		state |= std::istream::failbit;
-
-	s.setstate( state );
 	return s;
+}
+
+String trgm::operator+( const String& a, const String& b )
+{
+	return Concatenate( a.Data(), a.Length(), b.Data(), b.Length() );
+}
+
+String trgm::operator+( const String& a, const char* b )
+{
+	return Concatenate( a.Data(), a.Length(), b, CStrLength( b ) );
+}
+
+String trgm::operator+( const char* a, const String& b )
+{
+	return Concatenate( a, CStrLength( a ), b.Data(), b.Length() );
 }
 
 std::ostream& trgm::operator<<( std::ostream& s, const String& a )
@@ -105,7 +161,7 @@ bool comparators::CaseInsensitive( const String& a, const String& b )
 	const auto* aPtr = a.Data();
 	const auto* bPtr = b.Data();
 
-	for( auto n = std::min( a.Length(), b.Length() ); n--; aPtr++, bPtr++ )
+	for( size_t n = std::min( a.Length(), b.Length() ); n--; aPtr++, bPtr++ )
 	{
 		if( std::toupper( *aPtr ) < std::toupper( *bPtr ) )
 			return true;
--- string/String.hpp
+++ string/String.hpp
@@ -1,39 +1,48 @@
 #pragma once
-#include "StringBuffer.hpp"
 #include <istream> // std::istream
 #include <ostream> // std::ostream
 #include <cstddef> // size_t
 
 namespace trgm
 {
-	//	Simple ASCII string. Not null-terminated. Uses buffer with some capacity.
+	// Simple ASCII string. Not null-terminated.
 	class String
 	{
 	public:
 		String() = default;
+		~String();
+
+		String( const String& cstr );
+		String& operator=( const String& o );
+
+		String( String&& cstr );
+		String& operator=( String&& o );
+
 		explicit String( const char* cstr );
 		String& operator=( const char* o );
 
+		explicit String( size_t length, char value );
+
 		size_t		Length() const { return m_length; }
-		const char* Data() const { return m_buffer.Data(); } // NOTE: NOT null terminated
+		char*		Data() { return m_buffer; }		  // NOTE: NOT null terminated
+		const char* Data() const { return m_buffer; } // NOTE: NOT null terminated
 
 		String& operator+=( char c );
 		String& operator+=( const String& o );
 		String& operator+=( const char* o );
 
-	private:
 		// Copies chars from `src` of size `srcSize` into buffer at `position`.
-		// Increases buffer size and length if needed.
+		// Increases length if needed.
 		void Fill( const char* src, size_t srcSize, size_t position = 0 );
 
 	private:
-		size_t		 m_length = 0;
-		StringBuffer m_buffer = StringBuffer{};
+		char*  m_buffer = nullptr;
+		size_t m_length = 0;
 	};
 
-	inline String operator+( const String& a, const String& b ) { return ( String{} += a ) += b; }
-	inline String operator+( const String& a, const char* b ) { return ( String{} += a ) += b; }
-	inline String operator+( const char* a, const String& b ) { return ( String{} += a ) += b; }
+	String operator+( const String& a, const String& b );
+	String operator+( const String& a, const char* b );
+	String operator+( const char* a, const String& b );
 
 	std::istream& operator>>( std::istream& s, String& a );
 	std::ostream& operator<<( std::ostream& s, const String& a );
@@ -42,7 +51,6 @@ namespace trgm
 	{
 		bool CaseInsensitive( const String& a, const String& b );
 		bool CaseInsensitiveInverse( const String& a, const String& b );
-
 	} // namespace comparators
 
 } // namespace trgm
